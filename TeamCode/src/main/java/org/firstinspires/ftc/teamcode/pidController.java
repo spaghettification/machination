ba72263d8e086a6b1d4a5ab72e2e3869557931d1
spaghettification <tkgn15@live.com>
2016-10-31 09:33:50 -0500

    Create pidController.java

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/pidController.java
@@ -0,0 +1,54 @@
+public class PID{
+
+float proportionalConstant=0;
+float integralConstant=0;
+float derivitiveConstant=0;
+intTarget=100;
+float proportionalCorrection;
+float integralCorrection;
+float deriviiveCorrection;
+float Slopeofderivitive;
+float Cuumulativeerror=0;
+float maxCorrection=100;
+float minCorrection=15;
+
+public void Correction(){
+    while(opModeisActive()){
+    error=Math.abs(target-getIntegratedZheading);
+    proportionalCorrection=proportionalConstant*error;
+    
+    Cuumulativeerror+=error;
+    integralCorrection=integralConstant*cuumulativeerror;
+    
+    Slopeofderivitive=error-lasterror;
+    derivitivecorrection=Slopeofderivitive*derivitiveConstant;
+    lasterror=error
+    
+    Correction=ProportionalCorrectoin+integralCorrection+derivitivCorrection;
+    
+    if Correction>MaxCorrection{Correction=MaxCorrection}
+      else if (Correction<MinCorrection){Correction=MinCorrection}
+        else Correction=Correction
+    
+    if (Target>getIntegratedZheading){
+      FrontLeft.setPower(Power+Correction)
+      FrontRight.setPower(Power-Correction)
+      BackLeft.setPower(Power+Correction)
+      BackRight.setPower(Power-Correction)}
+    else if (Target<getIntegratedZheading){
+      FrontLeft.setPower(Power-Correction)
+      FrontRight.setPower(Power+Correction)
+      BackLeft.setPower(Power-Correction)
+      BackRight.setPower(Power+Correction)}
+    else {
+      FrontLeft.setPower(Power-Correction)
+      FrontRight.setPower(Power+Correction)
+      BackLeft.setPower(Power-Correction)
+      BackRight.setPower(Power+Correction)
+    }
+    }
+    
+    
+    }
+  }
+}
